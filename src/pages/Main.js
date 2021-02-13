504b68aeaa3ff22bc13a4d26c0963b56405676ca
jinwoo <hongdongdong3012@gmail.com>
2021-02-13 22:23:18 +0900

    준비

---
+++ src/pages/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div id="mainWrap">
+        
+      </div>
+    )
+  }
+}
+
+export default Main;
