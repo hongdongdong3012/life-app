8d8ffe7b01771973958cd4d7655483c6e29c3060
hongdongdong <hongdongdong3012@gmail.com>
2021-02-17 17:17:28 +0900

    UI 완료

--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -1,23 +1,47 @@
+import { useState } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactHtmlParser from 'react-html-parser';
 import '../styles/board.css';
 
   function Board() {
+    const [movieContent, setMovieContent] = useState({
+      title: '',
+      content: ''
+    })
+
+    const [viewContent, setViewContent] = useState([])
+
+    const getValue = e => {
+      const { name, value } = e.target;
+      setMovieContent({
+        ...movieContent,
+        [name]: value
+      })
+      console.log(movieContent);
+    }
+
+
+
     return (
       <section className="boardWrap">
         <h2>게시판</h2>
         <div className='container'>
           <div className="textGroup">
-            <h3>제목</h3>
-            <div class="contents">
-              내용
-            </div>
+            {viewContent.map(e => 
+              <>
+                <h3>{e.title}</h3>
+                <div className="contents">
+                  {ReactHtmlParser(e.content)}
+                </div>
+              </>
+            )}
           </div>
           <div className='formWrapper'>
-            <input className="title" type='text' placeholder='제목' />
+            <input className="title" type='text' placeholder='제목' onChange={getValue} name='title'/>
             <CKEditor
               editor={ClassicEditor}
-              data="<p>Hello from CKEditor 5!</p>"
+              data=""
               onReady={editor => {
                 // You can store the "editor" and use when it is needed.
                 console.log('Editor is ready to use!', editor);
@@ -25,6 +49,11 @@ import '../styles/board.css';
               onChange={(event, editor) => {
                 const data = editor.getData();
                 console.log({ event, editor, data });
+                setMovieContent({
+                  ...movieContent,
+                  content: data
+                })
+                console.log(movieContent);
               }}
               onBlur={(event, editor) => {
                 console.log('Blur.', editor);
@@ -33,7 +62,11 @@ import '../styles/board.css';
                 console.log('Focus.', editor);
               }}
             />
-            <button className="submitButton">입력</button>
+            <button className="submitButton"
+            onClick={() => {
+              setViewContent(viewContent.concat({...movieContent}))
+            }}
+            >입력</button>
           </div>
         </div>
       </section>
