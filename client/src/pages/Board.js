5e29919ee34d1de9c6191e272d378ebcd001e645
hongdongdong <hongdongdong3012@gmail.com>
2021-02-17 09:34:09 +0900

    UI 수정

--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react'
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import '../styles/board.css';
 
-class Board extends Component {
-
-  funcction() {
+  function Board() {
     return (
-      <div>
+      <section className="boardWrap">
         <h2>게시판</h2>
         <div className='container'>
-          <h3>제목</h3>
-          <div>
-            내용
+          <div className="textGroup">
+            <h3>제목</h3>
+            <div class="contents">
+              내용
+            </div>
+          </div>
+          <div className='formWrapper'>
+            <input className="title" type='text' placeholder='제목' />
+            <CKEditor
+              editor={ClassicEditor}
+              data="<p>Hello from CKEditor 5!</p>"
+              onReady={editor => {
+                // You can store the "editor" and use when it is needed.
+                console.log('Editor is ready to use!', editor);
+              }}
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                console.log({ event, editor, data });
+              }}
+              onBlur={(event, editor) => {
+                console.log('Blur.', editor);
+              }}
+              onFocus={(event, editor) => {
+                console.log('Focus.', editor);
+              }}
+            />
+            <button className="submitButton">입력</button>
           </div>
         </div>
-        <div className='form-wrapper'>
-        <input className="title-input" type='text' placeholder='제목' />
-        <CKEditor
-          editor={ClassicEditor}
-          data="<p>Hello from CKEditor 5!</p>"
-          onReady={editor => {
-            // You can store the "editor" and use when it is needed.
-            console.log('Editor is ready to use!', editor);
-          }}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            console.log({ event, editor, data });
-          }}
-          onBlur={(event, editor) => {
-            console.log('Blur.', editor);
-          }}
-          onFocus={(event, editor) => {
-            console.log('Focus.', editor);
-          }}
-        />
-        </div>
-        <button className="submit-button">입력</button>
-      </div>
+      </section>
     )
   }
-}
 
 export default Board;
